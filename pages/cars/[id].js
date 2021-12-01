bb3166f91230e60233809d6d6cd2fa249fd6502c
sagarvadalia <sv344@cornell.edu>
2021-11-30 23:19:48 -0500

    server side generated code

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -12,17 +12,7 @@ export default function Car({car}) {
     )
 
 }
-// export async function getServerSideProps({ params }) {
-//     const req = await fetch(`http://localhost:3000/${params.id}.json`);
-//     const data = await req.json();
-
-//     return {
-//         props: { car: data },
-//     }
-// }
-
-export async function getStaticProps({ params }) {
-
+export async function getServerSideProps({ params }) {
     const req = await fetch(`http://localhost:3000/${params.id}.json`);
     const data = await req.json();
 
@@ -31,17 +21,27 @@ export async function getStaticProps({ params }) {
     }
 }
 
-export async function getStaticPaths() {
+// export async function getStaticProps({ params }) {
 
-    const req = await fetch('http://localhost:3000/cars.json');
-    const data = await req.json();
+//     const req = await fetch(`http://localhost:3000/${params.id}.json`);
+//     const data = await req.json();
 
-    const paths = data.map(car => {
-        return { params: { id: car } }
-    })
+//     return {
+//         props: { car: data },
+//     }
+// }
 
-    return {
-        paths,
-        fallback: false
-    };
-}
+// export async function getStaticPaths() {
+
+//     const req = await fetch('http://localhost:3000/cars.json');
+//     const data = await req.json();
+
+//     const paths = data.map(car => {
+//         return { params: { id: car } }
+//     })
+
+//     return {
+//         paths,
+//         fallback: false
+//     };
+// }
